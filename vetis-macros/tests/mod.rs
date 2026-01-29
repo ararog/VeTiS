e5d0fd65f96b4256f438236cc3c2c009e1b7967d
Rogério Araújo <rogerio.araujo@gmail.com>
2026-01-29 12:18:42 -0300

    feat(vetis): make repo a cargo workspace, added vetis-macros

---
+++ vetis-macros/tests/mod.rs
@@ -0,0 +1,38 @@
+use deboa::request::get;
+use vetis::server::virtual_host::handler_fn;
+use vetis_macros::http;
+
+#[tokio::test]
+async fn test_http() -> Result<(), Box<dyn std::error::Error>> {
+    let handler = handler_fn(|req| async move {
+        Ok(vetis::Response::builder().body(http_body_util::Full::from("Hello, World!")))
+    });
+
+    let mut server =
+        http!(hostname => "localhost", port => 8080, interface => "0.0.0.0", handler => handler)
+            .await?;
+
+    server
+        .start()
+        .await?;
+
+    let client = deboa::Deboa::new();
+
+    let response = get("http://localhost:8080")?
+        .send_with(client)
+        .await?;
+
+    assert_eq!(response.status(), 200);
+    assert_eq!(
+        response
+            .text()
+            .await?,
+        "Hello, World!"
+    );
+
+    server
+        .stop()
+        .await?;
+
+    Ok(())
+}
